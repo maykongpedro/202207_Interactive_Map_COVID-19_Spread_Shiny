169289de7feb1e985f7a28f71241f3b16a80507e
Maykon G. Pedro <maykonglaffite@gmail.com>
2022-07-26 20:55:04 -0300

    merge data in the shapefile

--- data-raw/20220725_02-get-data.R
+++ data-raw/20220725_02-get-data.R
@@ -1,5 +1,8 @@
 
-
+# Script 2 - Get data
+# Objective: reading each useful data that was downloaded, cleaning it and joining
+# then in the shapefile to consolidate a base that will be used in shiny app.
+# Sorce: https://www.youtube.com/watch?v=eIpiL6y1oQQ&ab_channel=RockEDUScienceOutreach
 
 # Get data ----------------------------------------------------------------
 
@@ -45,7 +48,7 @@ case_rate_long <- case_rate|>
     # reshape data
     tidyr::pivot_longer(
         cols = caserate_10001:caserate_11697,
-        names_to = "modczta",
+        names_to = "modzcta",
         names_prefix = "caserate_",
         values_to = "case_rate"
     )
@@ -67,4 +70,71 @@ percent_positive_long <- percent_positive |>
     )
 
 percent_positive_long |> dplyr::glimpse() # see structure
- 
+
+
+# clean and reshape test_rates data
+test_rate |> dplyr::glimpse() # see structure
+
+test_rate_long <- test_rate |> 
+    dplyr::select(-c(testrate_city:testrate_si)) |> 
+    tidyr::pivot_longer(
+        cols = testrate_10001:testrate_11697,
+        names_to = "modzcta",
+        names_prefix = "testrate_",
+        values_to = "test_rate"
+    )
+
+test_rate_long |> dplyr::glimpse() # see structure
+
+
+
+# Merge in geography data -------------------------------------------------
+
+# join case_rate with percent positive + test rate
+all_data <- case_rate_long |> 
+    dplyr::left_join(
+        y = percent_positive_long,
+        by = c("week_ending", "modzcta")
+    ) |> 
+    dplyr::left_join(
+        y = test_rate_long,
+        by = c("week_ending", "modzcta")
+    )
+
+
+# merge covid data with czta shapefile
+all_modzcta <- modzcta |> 
+    
+    # function utilized in the video
+    # tigris::geo_join(
+    #     data_frame = all_data,
+    #     by_sp = "MODZCTA",
+    #     by_df = "modczta",
+    #     how = "inner"
+    # )
+    # R recommends using this function instead of the one above
+    dplyr::inner_join(
+        y = all_data,
+        by = c("MODZCTA" = "modczta")
+    )
+
+
+all_modzcta
+
+
+# Clear geography data ----------------------------------------------------
+
+all_modzcta |> dplyr::glimpse()  # see structure
+
+# convert date columns to class data
+all_modzcta <- all_modzcta |> 
+    dplyr::mutate(
+        week_ending = lubridate::mdy(week_ending)
+    )
+
+
+
+# Export data -------------------------------------------------------------
+
+# export in rds extension to use in shiny
+all_modzcta |> saveRDS("data/all_modzcta.rds")
